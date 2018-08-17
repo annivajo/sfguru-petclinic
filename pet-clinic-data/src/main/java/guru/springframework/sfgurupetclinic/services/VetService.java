a1b64b727e8a395538882f8234c0bdeb4c729a81
Ivanova Anna <annivajo@gmail.com>
2018-08-17 16:47:11 +0300

    Create Interfaces for Pet Clinic Services. Closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgurupetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgurupetclinic.services;
+
+import guru.springframework.sfgurupetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
