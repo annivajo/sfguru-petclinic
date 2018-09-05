2009245925ab5d725b70450c707f88e2fa71387b
Ivanova Anna <annivajo@gmail.com>
2018-09-05 10:32:08 +0300

    Add Specialty Service. Closes #31

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgurupetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgurupetclinic.services;
+
+import guru.springframework.sfgurupetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long>{
+}
