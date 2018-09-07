d54cf44bd42b355917e97753119f9013b80f69b7
Ivanova Anna <annivajo@gmail.com>
2018-09-07 11:00:35 +0300

    Convert Visit to JPA Entity. Closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgurupetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgurupetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgurupetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -15,11 +17,22 @@ public class Pet extends BaseEntity {
     private PetType petType;
 
     @ManyToOne
-    @JoinColumn(name="owner_id")
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
